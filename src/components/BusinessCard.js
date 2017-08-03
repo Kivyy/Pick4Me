46f24ed99131f38fc40b130c7056f75d6a7a8f70
Kiv Lo <kivlokw@gmail.com>
2017-08-03 14:34:09 -0400

    add conditional rendering to business card component

--- src/components/BusinessCard.js
+++ src/components/BusinessCard.js
@@ -7,22 +7,43 @@ class BusinessCard extends Component {
   constructor () {
     super()
 
+    this.state = {
+      yesToBar: false
+    }
+
+    this.handleYes = this.handleYes.bind(this);
   }
 
+  handleYes (e){
+    e.preventDefault();
+    this.setState({yesToBar: true});
+  }
 
   render() {
     const newCard = this.props.newCard
     const bar = this.props.selectedBar
     // console.log("this are the coordinates", bar["coordinates"])
-    if(bar.coordinates){
+
+
+    if(bar.coordinates && !this.state.yesToBar){
       return (
         <div>
           <p>{bar.coordinates.longitude}</p>
           <p>{bar.name}</p>
           <img src={bar.image_url} />
-          <button> X </button>
-          <button onClick={newCard}> Yes </button>
-            <GoogleMapContainer barLat={bar.coordinates.latitude} barLong={bar.coordinates.longitude} /> 
+          <button onClick={this.handleYes}> Yes </button>
+          <button onClick={newCard}> No </button>
+        </div>
+      )
+    } else if (this.state.yesToBar){
+      return (
+        <div>
+          <p>{bar.coordinates.longitude}</p>
+          <p>{bar.name}</p>
+          <img src={bar.image_url} />
+          <button onClick={this.handleYes}> Yes </button>
+          <button onClick={newCard}> No </button>
+          <GoogleMapContainer barLat={bar.coordinates.latitude} barLong={bar.coordinates.longitude} />
         </div>
       )
     } else {
